990b40f31a525f2300f6e0d3d86ae11f4a59c137
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-10-28 17:50:00 +0530

    styled with different methods

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,40 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { firestore } from "../firebase";
+import styled from "styled-components";
+
+const BlogHeading = styled.h1`
+  text-align: center;
+  color: #2196f3;
+  margin-bottom: 2px;
+`;
+
+const PostSubTitle = styled.p`
+  font-size: 13px;
+`;
+
+const Post = styled.div`
+  border: 1px solid #e1e1e1;
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+
+  h3 {
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+    font-weight: bold;
+    color: black;
+  }
+
+  h3:hover {
+    color: #2196f3;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -22,17 +56,17 @@ function Home() {
 
   return (
     <div className="home">
-      <h1>Tech Blog</h1>
+      <BlogHeading>Tech Blog</BlogHeading>
       <div id="blog-by">Krishna</div>
 
       {posts.map((post, index) => {
         return (
-          <div className="post" key={`post-${index}`}>
+          <Post className="post" key={`post-${index}`}>
             <Link to={`/post/${post.id}`}>
               <h3>{post.title}</h3>
             </Link>
-            <p>{post.subTitle}</p>
-          </div>
+            <PostSubTitle>{post.subTitle}</PostSubTitle>
+          </Post>
         );
       })}
     </div>
